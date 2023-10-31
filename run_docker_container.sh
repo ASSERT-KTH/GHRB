b54aa625a8ec3a2e8ce66dfde37e71749fb9a588
André Silva <andre15andre@hotmail.com>
2023-10-31 18:26:03 +0100

    change run_docker_container to receive command as an argument

--- run_docker_container.sh
+++ run_docker_container.sh
@@ -1,5 +1,7 @@
+#!/usr/bin/env bash
+
 containername="ghrb_framework"
 
 docker ps -aq --filter "name=$containername" | grep -q . && docker stop $containername && docker rm $containername
 docker run -dt --name $containername -v $(pwd):/root/framework ghrb_framework:latest
-docker exec -it $containername /bin/bash
+docker exec $containername $1
