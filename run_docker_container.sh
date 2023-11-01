a554ebd231ba138a84cf5ff130921c539634739f
André Silva <andre15andre@hotmail.com>
2023-11-01 14:06:29 +0100

    add volume for tmp dir

--- run_docker_container.sh
+++ run_docker_container.sh
@@ -3,5 +3,6 @@
 containername="ghrb_framework"
 
 docker ps -aq --filter "name=$containername" | grep -q . && docker stop $containername && docker rm $containername
-docker run -dt --name $containername -v $(pwd):/root/framework ghrb_framework:latest
+mkdir -p /tmp/elleelleaime/ghrb/
+docker run -dt --name $containername -v $(pwd):/root/framework -v /tmp/elleelleaime/ghrb/:/tmp/elleelleaime/ghrb/ ghrb_framework:latest
 docker exec $containername $1
