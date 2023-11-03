67a5099aea0533ba7e19d9f6a1c8c3d9af2ad9b8
André Silva <andre15andre@hotmail.com>
2023-11-03 17:02:51 +0100

    fix tmp dir

--- run_docker_container.sh
+++ run_docker_container.sh
@@ -4,5 +4,5 @@ containername="ghrb_framework"
 
 docker ps -aq --filter "name=$containername" | grep -q . && docker stop $containername && docker rm $containername
 mkdir -p /tmp/elleelleaime/ghrb/
-docker run -dt --name $containername -v $(pwd):/root/framework -v /tmp/elleelleaime/ghrb/:/tmp/elleelleaime/ghrb/ ghrb_framework:latest
+docker run -dt --name $containername -v $(pwd):/root/framework -v /tmp/elleelleaime/:/tmp/elleelleaime/ ghrb_framework:latest
 docker exec $containername $1
