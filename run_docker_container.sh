7255e1fcfe0a246ce71feec18f60a9d86b1635c8
André Silva <andre15andre@hotmail.com>
2023-11-03 17:20:11 +0100

    fix path

--- run_docker_container.sh
+++ run_docker_container.sh
@@ -3,6 +3,6 @@
 containername="ghrb_framework"
 
 docker ps -aq --filter "name=$containername" | grep -q . && docker stop $containername && docker rm $containername
-mkdir -p /tmp/elleelleaime/ghrb/
+mkdir -p /tmp/elleelleaime/
 docker run -dt --name $containername -v $(pwd):/root/framework -v /tmp/elleelleaime/:/tmp/elleelleaime/ ghrb_framework:latest
 docker exec $containername $1
